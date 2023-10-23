631ccf05fc6f31c5dcd5bbb6484a1f82c4b72b65
ozturk sirin <ozturk2002.zri@gmail.com>
2023-10-23 20:51:44 +0300

    inital commit

---
+++ src/Router/Router.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native';
+
+const Router = () => {
+    return (
+        <>
+            <Text>Router</Text>
+
+        </>
+    )
+}
+
+export default Router
